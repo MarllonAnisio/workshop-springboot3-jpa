172bab8c25f7005c691ef7ab3ee44b90366fedbe
Marllon Anisio <marllon.anisio@hotmail.com>
2025-03-04 21:48:41 -0300

    Category

---
+++ src/main/java/com/MarllonAnisio/curso/services/CategoryService.java
@@ -0,0 +1,25 @@
+package com.MarllonAnisio.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.MarllonAnisio.curso.entities.Category;
+import com.MarllonAnisio.curso.repositories.CategoryRepository;
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();	
+	}
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
