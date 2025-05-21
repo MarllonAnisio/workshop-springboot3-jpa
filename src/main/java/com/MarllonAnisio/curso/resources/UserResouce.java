fabfbd86c6eadf38449d4a86c2ea4c6e800a4d02
Marllon Anisio <marllon.anisio@hotmail.com>
2025-05-21 15:31:00 -0300

    User Insert

--- src/main/java/com/MarllonAnisio/curso/resources/UserResouce.java
+++ src/main/java/com/MarllonAnisio/curso/resources/UserResouce.java
@@ -1,16 +1,15 @@
 package com.MarllonAnisio.curso.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.MarllonAnisio.curso.entities.User;
 import com.MarllonAnisio.curso.services.UserService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -29,5 +28,14 @@ public class UserResouce {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User user) {
+		user = service.insertUser(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).body(user);
+	}
+
+
 
 }
